fe99217a09ce782ea6cf403717dde8843fcf09f6
Patricio Ramirez <pramirez@ip100.0.priv.inf.utfsm.cl>
2015-12-11 14:17:01 -0300

    quantity, amount

--- app/models/user/model.py
+++ app/models/user/model.py
@@ -9,6 +9,7 @@ class User(Model):
     id       = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     quantity = db.Column(db.Integer,default=0)
+    amount   = db.Column(db.Integer,default=0)
     
     created_at = db.Column(db.DateTime(timezone=False),default=datetime.utcnow)
     updated_at = db.Column(db.DateTime(timezone=False),default=datetime.utcnow)
